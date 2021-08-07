49739169f29963b59ef6c3b8c44916d5a7856d5a
Amar K <amarkhamkar6@gmail.com>
2021-08-07 17:32:39 +0530

    Implemented everything till section-2"

--- Assignment_code.py
+++ Assignment_code.py
@@ -20,6 +20,7 @@ try:
     total_rows=df.shape[0]
     per_student_offset=25
     for Alls in range(0,total_rows//per_student_offset):
+
         Alls_marks.append(df['Your score'][Alls*per_student_offset:Alls*per_student_offset+25].sum())
         Alls_register.append(df['Registration Number'][Alls*per_student_offset])
 
@@ -30,27 +31,43 @@ try:
                 count+=1
         
         return (count / 5)*100
-    def drawPieChart(pc,d,x,y,width,height,data_str,labels,can,data):
-        pc.x = x
-        pc.y = y
-        pc.width = width
-        pc.height = height
-        pc.data = data
-        pc.labels = data_str
-        pc.slices.strokeWidth=0.5
-        d.add(pc)
-        d.drawOn(can,0,0)
+    
+    def Drawtable(detail,x,y,can,colwid):
+        tbl_style = (
+            ('FONT', (0, 0), (0, -1), "Helvetica-Bold"),
+            ('FONT', (3, 0), (3, -1), "Helvetica-Bold"),
+            ('INNERGRID', (0, 0), (1, -1), 1, (0, 0, 0)),
+            ('INNERGRID', (-2, 0), (-1, -1), 1, (0, 0, 0)),
+            ('BOX', (0, 0), (1, -1), 1, (0, 0, 0)),
+            ('BOX', (-2, 0), (-1, -1), 1, (0, 0, 0)),
+        )
+        table = Table(detail, style=tbl_style, colWidths=colwid)
+        w, h = table.wrapOn(can, 0, 0)
+        table.drawOn(can,x,y)
+    
+    def Drawbargraph(data,labels,x,y,width,height,can):
+        d=Drawing(400,200)
+        bc = VerticalBarChart()
         
-        can.setFont("Helvetica-Oblique",10)
-        can.setFillColor(colors.green)
-        y-=30
-        can.drawString(x+10,y,"Total Qs : 25")
-        y-=30
-        
-        for i in range(len(labels)):
-            can.drawString(x+10,y,f"{labels[i]} : {data[i]}")
-            y-=30
-        
+        bc.x = x
+        bc.y = y
+        bc.height = height
+        bc.width = width
+        bc.data = [(data)]
+        bc.strokeColor = colors.black
+        bc.bars[0].fillColor = colors.blue
+        bc.valueAxis.valueMin = 0
+        bc.valueAxis.valueMax = 100
+        bc.valueAxis.valueStep = 30
+        bc.categoryAxis.labels.boxAnchor = 'ne'
+        bc.categoryAxis.labels.dx = 8
+        bc.categoryAxis.labels.dy = -2
+        bc.categoryAxis.labels.angle = 30
+        bc.categoryAxis.categoryNames = labels
+        d.add(bc)
+        # d.wrapOn(can,400,200    can=
+        d.drawOn(can,10,10)
+
     def create_pdf(pdf_file,row_range,col_range_for_table,row_init,st1_details,bg_img,cont,cont_1):
         
         can = canvas.Canvas(pdf_file,pagesize=pagesizes.A4)
@@ -60,6 +77,10 @@ try:
         logo_img="logo.png"
         pathSt_img=f"static/Pics for assignment/{st1_details['Sname'][row_init]}.png"
         can.drawImage(bg_img,0,0,width=width,height=height , preserveAspectRatio=True, mask='auto')
+        can.setFont("Helvetica",10)
+        can.drawString(10,970, f"Round 2 - enhanced Score Report : {st1_details['Sname'][row_init]}")
+        can.drawString(10,950, f"Registration Number : {int(st1_details['RegisNo'][row_init])}")
+
         #title
         can.setFillColor(colors.black)
         can.setFont("Helvetica-Bold",15)
@@ -75,17 +96,9 @@ try:
         ("Country Of Residence", f"{st1_details['country'][row_init]}", "", "Date Of Test",
          f"5-6 august 2021")
         )
-        tbl_style = (
-            ('FONT', (0, 0), (0, -1), "Helvetica-Bold"),
-            ('FONT', (3, 0), (3, -1), "Helvetica-Bold"),
-            ('INNERGRID', (0, 0), (1, -1), 1, (0, 0, 0)),
-            ('INNERGRID', (-2, 0), (-1, -1), 1, (0, 0, 0)),
-            ('BOX', (0, 0), (1, -1), 1, (0, 0, 0)),
-            ('BOX', (-2, 0), (-1, -1), 1, (0, 0, 0)),
-        )
-        table = Table(student_detail, style=tbl_style, colWidths=(None, 5 * cm, 1 * cm, None, 5 * cm))
-        w, h = table.wrapOn(can, 0, 0)
-        table.drawOn(can,130,630)
+        
+        Drawtable(student_detail,130,630,can,(None, 5 * cm, 1 * cm, None, 5 * cm))
+
         can.setFont("Helvetica-Oblique",15)
         can.drawString(370, 580, "Section - 1")
         can.setFont("Helvetica",12)
@@ -165,156 +178,57 @@ try:
         table2 = Table(report_table_data_1, style=style)
         w, h = table2.wrapOn(can, 0, 0)
         table2.drawOn(can,40,380)
-        print(score,count_percentile(score))
-        
-        
-        # can.setFont("Helvetica-Oblique",30)
-        # can.setFillColor(colors.red)
 
-        # can.drawString(100,800,"Student Scorecard and analysis")
-        # can.setFillColor(colors.orange)
-        # can.setFont("Helvetica-Oblique",10)
+        can.setFont("Helvetica",10)
+        can.drawString(20, 360, f"{st1_details['Sname'][row_init]} overall percentile is {count_percentile(score)}% . This indicates that {st1_details['Sname'][row_init]} has scored more than {count_percentile(score)}%  of students in the World and lesser than {100 - count_percentile(score)}% of student in the world")
+        can.setFont("Helvetica-Bold",15)
+        can.drawString(50,320,"Overview")
 
-        # Total_marks=100
-        # Total_score_student=df['Your score'][row_init:row_init+25].sum()
-        # corrected_answer=0
-        # uncorrected_ans=0
-        # Not_attempted=0
-        # for outc in df['Outcome (Correct/Incorrect/Not Attempted)'][row_init:row_init+25]:
-        #     if outc=="Correct":
-        #         corrected_answer+=1
-        #     elif outc=="Incorrect":
-        #         uncorrected_ans+=1
-        #     else:
-        #         Not_attempted+=1
-        
 
-        # can.setFont("Helvetica-Oblique",15)
-        # can.setFillColor(colors.orange)
-        # can.drawString(180,750,f"Student Score :  {Total_score_student} / 100")
-        # can.drawString(100,715,f"{df['Qualification'][row_init]}")
-        # can.setFillColor(colors.black)
-        # can.line(30,705,550,705)
-        # #Analysis using piechart
-    
-        # can.setFont("Helvetica-Oblique",15)
-        # can.setFillColor(colors.black)
-        # can.drawString(80,680,"Question Analysis")
-        # d = Drawing(200, 100)
-    
-        # pc = Pie()
-    
-        # pc.sideLabels=True
-        # x1=80
-        # y1=550
-        # width1=100
-        # height1=100
-        # data1=[Not_attempted,25-Not_attempted]
-        # data_str1=[str(Not_attempted),str(25-Not_attempted)]
-        # labels1=['Unattempted Qs',"Attempted Qs"]
-        # drawPieChart(pc,d,x1,y1,width1,height1,data_str1,labels1,can,data1)
+        #table and its respective bar graph
         
-        # ##Piechart 2
-        # can.setFillColor(colors.black)
-        # can.setFont("Helvetica-Oblique",15)
-        # can.drawString(400,680,"Solution Analysis")
-        # x1=400
-        # y1=545
-        # width1=100
-        # height1=100
-        # data1=[uncorrected_ans,corrected_answer,Not_attempted]
-        # data_str1=[str(uncorrected_ans),str(corrected_answer),str(Not_attempted)]
-        # labels1=["Uncorrect answers","Correct answers","Not attempted"]
-        # drawPieChart(pc,d,x1,y1,width1,height1,data_str1,labels1,can,data1)
+        Avg_score = df['Average score of all students across the World'][row_init]
+        Median = df['Median score of all students across the World'][row_init]
+        Mode = df['Mode score of all students across World'][row_init]
+        Data_bar_1 = [score , Avg_score , Median , Mode]
+        Labels_1 = ['You' , 'Avg' , 'Median' , 'Mode']
+        detail_1 = (
+        ("Average score of all\nstudents across the World ", f"{Avg_score}"),
+        ("Median score of all\nstudents across the World ", f"{Median}"),
+        ("Mode score of all students across World ", f"{Mode}"),
+        )
         
-        # ##Comparision bar graph
+        Drawtable(detail_1,30,230,can,(None, 1.2 * cm))
+        can.setFont("Helvetica",12)
+        can.drawString(80,200,"Comparision Of Scores")
+        Drawbargraph(Data_bar_1 , Labels_1 ,55,20,150,150,can)
 
-        # can.setFillColor(colors.black)
-        # can.setFont("Helvetica-Oblique",25)
-        # can.drawString(230,350,"Comparision")
-        # data_all = []
-        # names_list=[]
-        # for j in range(len(Alls_register)):
-        #     if Alls_register[j]!=st1_details['RegisNo'][row_init]:
-        #         data_all.append(Alls_marks[j])
-        #         names_list.append(df['Full Name '][j*25])
-            
 
-        # data_all.insert(0,Total_score_student)
-        # names_list.insert(0,"You")
-        # # print(Alls_names)
-        # d=Drawing(400,200)
-        # bc = VerticalBarChart()
+        stu_attempt = df['First names attempts (Attempts x 100 / Total Questions)'][row_init]
+        Avg_attempt = df['Average attempts of all students across the Worl'][row_init]
+        Data_bar_2 = [stu_attempt , Avg_attempt]
+        Labels_2 = ['You' , 'World']
+        detail_2 = (
+        (f"{st1_details['Sname'][row_init]}'s attempts\n(Attempts x 100 / Total Questions)  ", f"{stu_attempt}"),
+        ("Average attempts of all\nstudents across the World ", f"{Avg_attempt}"),
+        )
         
-        # bc.x = 150
-        # bc.y = 100
-        # bc.height = 300
-        # bc.width = 300
-        # bc.data = [(data_all)]
-        # bc.strokeColor = colors.black
-        # bc.valueAxis.valueMin = 0
-        # bc.valueAxis.valueMax = 100
-        # bc.valueAxis.valueStep = 30
-        # bc.categoryAxis.labels.boxAnchor = 'ne'
-        # bc.categoryAxis.labels.dx = 8
-        # bc.categoryAxis.labels.dy = -2
-        # bc.categoryAxis.labels.angle = 30
-        # bc.categoryAxis.categoryNames = names_list
-        # d.add(bc)
-        # # d.wrapOn(can,400,200    can=
-        # d.drawOn(can,10,10)
-        
-        # print(ca)
-        can.showPage()
+        Drawtable(detail_2,280,240,can,(None, 3 * cm))
+        can.drawString(300,200,"Comparision Of Attempt(%)")
+        Drawbargraph(Data_bar_2 , Labels_2 ,330,20,100,150,can)
 
-    
-        ##***********************************************PAGE3********************************************* 
-        ##if student want to see records and details of each question then records of table can be displayed by folowing code
-        ##all details of exam with marked qs and its response
-        can.setFillColor(colors.red)
-        can.setFont("Helvetica-Oblique",30)
-        can.drawString(150,800, "Student Test records")
-        can.setFont("Helvetica-Oblique",12)
-        can.setFillColor(colors.orange)
+        Stu_accu = df['First names Accuracy ( Corrects x 100 /Attempts )'][row_init]
+        Avg_acc = df['Average accuracy of all students across the World'][row_init]
+        Data_bar_3 = [float(str(Stu_accu)[:len(str(Stu_accu)) - 2]) , float(str(Avg_acc)[:len(str(Avg_acc)) - 2])]
+        Labels_3 = ['You' , 'World']
+        detail_3 = (
+        (f"{st1_details['Sname'][row_init]}'s Accuracy\n( Corrects x 100 /Attempts ) ", f"{Stu_accu}"),
+        ("Average accuracy of all\nstudents across the World ", f"{Avg_acc}"),
+        )
         
-        can.drawString(50,750, "Below there are the responses of each question which was in the test given by the student. ")
-        
-        
-        #column list
-        can.setFillColor(colors.red)
-
-        column_name=['Qs no','Marked ans' ,'correct ans' , 'Outcome' , 'score if correct' ,'Your score']##shortend names of columns due to unavailable space
-        ##declaring variables 
-        
-        col_x_value=75
-        col1_x_value=70
-        table_y1_top_value=720
-        table_x2_right_value=590
-        can.line(col1_x_value, table_y1_top_value, table_x2_right_value, table_y1_top_value)
-        for colm in column_name:
-            # print(colm)
-            can.drawString(col_x_value,700, colm)
-            col_x_value+=90
-        table_x_value=80
-        can.line(col1_x_value, 685, table_x2_right_value, 685)
-        can.setFillColor(colors.green)
-        for nexCol_value in range(len(col_range_for_table)):
-            table_y_value=670
-            row=[]
-            for nexRow_value in range(row_init,row_init+25):
-                value=str(row_range[col_range_for_table[nexCol_value]][nexRow_value])
-                can.drawString(table_x_value, table_y_value, value)#value placed in given position
-                table_y_value-=20
-                can.line(col1_x_value, table_y_value+15, table_x2_right_value, table_y_value+15)##these are to create table outline
-            can.line(table_x_value-10, table_y1_top_value, table_x_value-10, 180)
-            
-            table_x_value+=90
-        can.line(table_x2_right_value, table_y1_top_value, table_x2_right_value, 180)
-        
-        can.showPage()
-    
-        
-    
+        Drawtable(detail_3,550,240,can,(None, 3 * cm))
+        can.drawString(540,200,"Comparision Of Accuracy(%)")
+        Drawbargraph(Data_bar_3 , Labels_3 ,600,20,100,150,can)
         can.save()
 
 
